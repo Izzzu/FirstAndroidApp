914c1583dfae17cc9c03492094dbc67ccee55058
Izabela Kułakowska <izabela@Izabelas-iMac.local>
2017-08-01 19:56:06 +0200

    Photo taking

--- app/src/main/java/com/example/izabela/firstapp/MainActivity.java
+++ app/src/main/java/com/example/izabela/firstapp/MainActivity.java
@@ -1,20 +1,63 @@
 package com.example.izabela.firstapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private static final int TAKE_PHOTO_CODE = 1;
+    private ImageView mImageView;
+    private int count;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Here, we are making a folder named picFolder to store
+        // pics taken by the camera using this application.
+        final String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/picFolder/";
+        File newdir = new File(dir);
+        newdir.mkdirs();
+
+        Button capture = (Button) findViewById(R.id.btnPhoto);
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            public void onClick(View v) {
+
+                // Here, the counter will be incremented each time, and the
+                // picture taken by camera will be stored as 1.jpg,2.jpg
+                // and likewise.
+                count++;
+                String file = dir + count + ".jpg";
+                File newfile = new File(file);
+                try {
+                    newfile.createNewFile();
+                } catch (IOException e) {
+                }
+
+                Uri outputFileUri = Uri.fromFile(newfile);
+
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+                startActivityForResult(cameraIntent, TAKE_PHOTO_CODE);
+            }
+        };
+        capture.setOnClickListener(onClickListener);
     }
 
     /** Called when the user taps the Send button */
@@ -34,4 +77,15 @@ public class MainActivity extends AppCompatActivity {
         intent.setType("text/plain");
         startActivity(Intent.createChooser(intent, getResources().getText(R.string.send_to)));
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TAKE_PHOTO_CODE && resultCode == RESULT_OK) {
+            Log.d("CameraDemo", "Pic saved");
+        }
+    }
 }
+
